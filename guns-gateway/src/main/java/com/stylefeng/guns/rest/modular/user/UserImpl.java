2fe62205c5de258ab0241bbdcba1325d3cf5830c
胡方舟 <hfzh618@gmail.com>
2019-11-25 14:34:32 +0800

    网关模块的UserController实现 4.14-16

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/user/UserImpl.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/user/UserImpl.java
@@ -2,6 +2,8 @@ package com.stylefeng.guns.rest.modular.user;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.stylefeng.guns.api.user.UserAPI;
+import com.stylefeng.guns.api.user.UserInfoModel;
+import com.stylefeng.guns.api.user.UserModel;
 import org.springframework.stereotype.Component;
 
 /**
@@ -12,7 +14,27 @@ import org.springframework.stereotype.Component;
 @Service(interfaceClass = UserAPI.class )
 public class UserImpl implements UserAPI {
     @Override
-    public boolean login(String username, String password) {
-        return true;
+    public int login(String username, String password) {
+        return 1;
+    }
+
+    @Override
+    public boolean register(UserModel userModel) {
+        return false;
+    }
+
+    @Override
+    public boolean checkUsername(String username) {
+        return false;
+    }
+
+    @Override
+    public UserInfoModel getUserInfo(int uuid) {
+        return null;
+    }
+
+    @Override
+    public UserInfoModel updateUserInfo(UserInfoModel userInfoModel) {
+        return null;
     }
 }
--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/vo/ResponseVO.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/vo/ResponseVO.java
@@ -15,6 +15,13 @@ public class ResponseVO<M> {
         responseVO.setData(m);
         return responseVO;
     }
+
+    public static<M> ResponseVO success(String msg){
+        ResponseVO responseVO = new ResponseVO();
+        responseVO.setStatus(0);
+        responseVO.setMsg(msg);
+        return responseVO;
+    }
     public static<M> ResponseVO serviceFail(String msg){
         ResponseVO responseVO = new ResponseVO();
         responseVO.setStatus(1);
